d942c0c20b2957fa60ed169b1654f17a09e548b3
MiniSilveira <filipepsilveira2003@gmail.com>
2024-11-17 21:00:18 +0000

    Update SO

--- feed/src/feed.c
+++ feed/src/feed.c
@@ -5,7 +5,12 @@ int main(int argc, char *argv[]) {
 
   /****************** VERIFICAÇÃO E TRATAMENTO DE ARGUMENTOS ******************/
 
-  // código...
+  if (argc != 2 ){
+printf("Numero de argumentos incorreto\n");
+return 1;
+}
+
+ char *nome = argv[1];
 
   /************ INICIA PROCESSOS PARA RECEÇÃO E ENVIO DE MENSAGEMS ************/
   /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
@@ -15,7 +20,14 @@ int main(int argc, char *argv[]) {
    * mensagens e outro que trate da receção
    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-  // código...
+if(access ( FIFO_SRV, F_OK)!=0){
+printf("[ERRO] Servidor nao esta a correr!\n");
+exit(3);
+}
+sprintf (fifo_cli,  FIFO_CLI, getpid());
+mkfifo( fifo_cli , 0600);
+fd_cli = open (fifo_cli,O_RDWR);
+fd = open ( FIFO_SRV, O_WRONLY);
 
   /****************** REALIZA O ENVIO E RECEÇÃO DE MENSAGEMS ******************/
   /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
@@ -23,6 +35,17 @@ int main(int argc, char *argv[]) {
    * o código mais legível e organizado, será boa ideia escrever o código
    * para cada uma destas funções em ficheiros separados
    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+  do{
+scanf("%s", str);
+p.pid = getpid();
+strcpy(p.str, str);
+res  = write ( fd, &p , sizeof (PEDIDO));
+if ( res == sizeof(PEDIDO)){
+printf("ENVIEI  ... '%s' - %d ( %d bytes) \n", p.str,p.pid,res);
+res = read (fd_cli, &r, sizeof(RESPOSTA));
+printf("RECEBI... '%s' (%d)\n", r.str, res);
+}
+}while ( strcmp(str,"fim")!=0 && strcmp(str, "quit")!=0);
   if (0 /*recetor de mensagens?*/) {
     while (1) {
       /*************************** AGUARDA MENSAGEM ***************************/
@@ -60,5 +83,9 @@ int main(int argc, char *argv[]) {
    * Caso um dos processos (recetor ou emissor) tenha terminado, ambos os
    * processos são terminados.
    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+  printf("FIM! \n");
+close(fd);
+close(fd_cli);
+unlink (fifo_cli);
   exit(0);
 }
