21e050ce5d16ede843f259e64d3632ac2201b02d
André Maltez Nunes <andremalteznunes@sapo.pt>
2024-11-18 01:00:31 +0000

    Added comments defining the message structure Added a long list of specifications of all kinds of messages that will be traded between the manager and all of the feeds; Rewrote the msg struct to allow flexibility for all kinds of messages with a single defined struct; Added includes used by both programs to the globals.h, so these don't have to be repeated in both programs; Formatted the feed.c and manager.c; Temporatily commented out some code in feed.c and manager.c for the sake of compilation, given that some parts of the code will need to be adapted to the structure of this project.

--- feed/src/feed.c
+++ feed/src/feed.c
@@ -1,3 +1,4 @@
+#include "../../include/globals.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -5,12 +6,12 @@ int main(int argc, char *argv[]) {
 
   /****************** VERIFICAÇÃO E TRATAMENTO DE ARGUMENTOS ******************/
 
-  if (argc != 2 ){
-printf("Numero de argumentos incorreto\n");
-return 1;
-}
+  if (argc != 2) {
+    printf("Numero de argumentos incorreto\n");
+    exit(1);
+  }
 
- char *nome = argv[1];
+  char *nome = argv[1];
 
   /************ INICIA PROCESSOS PARA RECEÇÃO E ENVIO DE MENSAGEMS ************/
   /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
@@ -20,14 +21,18 @@ return 1;
    * mensagens e outro que trate da receção
    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-if(access ( FIFO_SRV, F_OK)!=0){
-printf("[ERRO] Servidor nao esta a correr!\n");
-exit(3);
-}
-sprintf (fifo_cli,  FIFO_CLI, getpid());
-mkfifo( fifo_cli , 0600);
-fd_cli = open (fifo_cli,O_RDWR);
-fd = open ( FIFO_SRV, O_WRONLY);
+  if (access(FIFO_SRV, F_OK) != 0) {
+    printf("[ERRO] Servidor nao esta a correr!\n");
+    exit(3);
+  }
+
+  char *fifo_cli;
+  sprintf(fifo_cli, FIFO_CLI, getpid());
+  mkfifo(fifo_cli, 0600);
+
+  int fd_cli, fd;
+  fd_cli = open(fifo_cli, O_RDWR);
+  fd = open(FIFO_SRV, O_WRONLY);
 
   /****************** REALIZA O ENVIO E RECEÇÃO DE MENSAGEMS ******************/
   /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
@@ -35,17 +40,18 @@ fd = open ( FIFO_SRV, O_WRONLY);
    * o código mais legível e organizado, será boa ideia escrever o código
    * para cada uma destas funções em ficheiros separados
    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-  do{
-scanf("%s", str);
-p.pid = getpid();
-strcpy(p.str, str);
-res  = write ( fd, &p , sizeof (PEDIDO));
-if ( res == sizeof(PEDIDO)){
-printf("ENVIEI  ... '%s' - %d ( %d bytes) \n", p.str,p.pid,res);
-res = read (fd_cli, &r, sizeof(RESPOSTA));
-printf("RECEBI... '%s' (%d)\n", r.str, res);
-}
-}while ( strcmp(str,"fim")!=0 && strcmp(str, "quit")!=0);
+  char str[30];
+  do {
+    scanf("%s", str);
+    // p.pid = getpid();
+    // strcpy(p.str, str);
+    // res = write(fd, &p, sizeof(PEDIDO));
+    // if (res == sizeof(PEDIDO)) {
+    //   printf("ENVIEI  ... '%s' - %d ( %d bytes) \n", p.str, p.pid, res);
+    //   res = read(fd_cli, &r, sizeof(RESPOSTA));
+    //   printf("RECEBI... '%s' (%d)\n", r.str, res);
+    // }
+  } while (strcmp(str, "fim") != 0 && strcmp(str, "quit") != 0);
   if (0 /*recetor de mensagens?*/) {
     while (1) {
       /*************************** AGUARDA MENSAGEM ***************************/
@@ -84,8 +90,8 @@ printf("RECEBI... '%s' (%d)\n", r.str, res);
    * processos são terminados.
    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
   printf("FIM! \n");
-close(fd);
-close(fd_cli);
-unlink (fifo_cli);
+  close(fd);
+  close(fd_cli);
+  unlink(fifo_cli);
   exit(0);
 }
--- manager/src/manager.c
+++ manager/src/manager.c
@@ -9,36 +9,36 @@
 #define MAX_PERSISTENT_MSGS 5
 
 int main(int argc, char *argv[]) {
- int fd,res;
-   char str[TAM];
-PEDIDO p;
-char fifo_cli[30];
-int fd_cli;
-RESPOSTA r;
+  int fd, res;
+  char str[30];
+  // PEDIDO p;
+  char fifo_cli[30];
+  int fd_cli;
+  // RESPOSTA r;
 
-   printf("INICIO!!! \n");
+  printf("INICIO!!! \n");
 
-if(access ( FIFO_SRV,F_OK)==0){
-printf("[ERRO] Ja existe um servidor!\n");
-exit(3);
-}
-   mkfifo(FIFO_SRV, 0600);
-   fd = open(FIFO_SRV, O_RDWR);
+  if (access(FIFO_SRV, F_OK) == 0) {
+    printf("[ERRO] Ja existe um servidor!\n");
+    exit(3);
+  }
+  mkfifo(FIFO_SRV, 0600);
+  fd = open(FIFO_SRV, O_RDWR);
 
+  /*
    do {
-      res = read(fd,&p,sizeof(PEDIDO));
+   // res = read(fd, &p, sizeof(PEDIDO));
 
-
-	sprintf( fifo_cli, FIFO_CLI, p.pid);
-	fd_cli = open (fifo_cli, O_WRONLY);
-	strcpy ( r.str,"OK");
-	res = write ( fd_cli, &r , sizeof(RESPOSTA));
-	printf( " ENVIEI ... '%s'(%d)\n", r.str, res);
-	close(fd_cli);
- }
-   }while ( strcmp(p.str,"quit") != 0 );
-      close(fd);
-       unlink(FIFO_SRV);
-      printf("FIM \n");
-       exit(0);
+   // sprintf(fifo_cli, FIFO_CLI, p.pid);
+   fd_cli = open(fifo_cli, O_WRONLY);
+   // strcpy(r.str, "OK");
+   // res = write(fd_cli, &r, sizeof(RESPOSTA));
+   // printf(" ENVIEI ... '%s'(%d)\n", r.str, res);
+   close(fd_cli);
+} while (strcmp(p.str, "quit") != 0);
+  */
+  close(fd);
+  unlink(FIFO_SRV);
+  printf("FIM \n");
+  exit(0);
 }
